72c9be16f87ccff1ee2dd8d840617acad3d46dc0
IvaniaJ <77988476+IvaniaJ@users.noreply.github.com>
2023-04-08 12:36:49 -0500

    Delete lab3/unions directory

--- lab3/unions/example1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-/* function which moves a point forward (move)*/
-
-void move(point * p) {
-    (*p).x++;
-    (*p).y++;
-}
--- lab3/unions/example2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/* dereferencing a structure and access internal members*/
-
-void move(point *p) {
-    p->x++;
-    p->y++;
-}
